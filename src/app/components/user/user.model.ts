07c2dbd381fd0fd056aadc7ef4e1b6d07078122c
Geovana Melo <geovanamoraisdemelo@gmail.com>
2020-11-20 12:08:52 -0300

    Modelo de dados do Usuario.

---
+++ src/app/components/user/user.model.ts
@@ -0,0 +1,15 @@
+export interface UserModel{
+
+  id: number;
+  nome: string;
+  apelido: string;
+  nascimento: string;
+  codigo: string;
+  sexo: string;
+  email: string;
+  estado: string;
+  cidade: string;
+  acessosCurso: number;
+  situacaoCurso: string;
+  inicioCurso: string;
+}
