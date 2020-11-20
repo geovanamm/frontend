2cddf2328dd2c7426ccc2fa956d52ad6462f8074
Geovana Melo <geovanamoraisdemelo@gmail.com>
2020-11-20 15:08:12 -0300

    Criando rota para a lista de usuarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserListComponent} from './components/user/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: UserListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
